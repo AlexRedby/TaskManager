4c67d4ffebe225595afd94b648a9308095e9f261
AlexRedby <ak765akya852@mail.ru>
2018-02-20 12:39:21 +0400

    now TaskList is saved to JSON file.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 package src;
 
+import src.controller.Controller;
 import src.controller.TaskList;
 import src.model.Task;
 import src.view.MainFrame;
 
 
 import javax.swing.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class Main {
     public static void main(String[] args) {
-        TaskList taskList = new TaskList();
+        TaskList taskList = null;
+        try {
+            taskList = Controller.readTaskList();
+        } catch (IOException e) {
+            taskList = new TaskList();
+        }
         JFrame frame = new MainFrame(taskList);
     }
 }
